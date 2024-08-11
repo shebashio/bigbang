d01439de3bde58d287ac15832b07e0f7d63db750
Greg <miernicki_gregory@bah.com>
2024-08-11 09:13:04 -0500

    ⛵

--- docs/assets/scripts/developer/k3d-dev.sh
+++ docs/assets/scripts/developer/k3d-dev.sh
@@ -756,10 +756,10 @@ spec:
   serviceAllocation:
     priority: 100
     namespaces:
-      - istio-system
+      - istio-ingress
     serviceSelectors:
       - matchExpressions:
-          - {key: app, operator: In, values: [public-ingressgateway]}
+          - {key: app, operator: In, values: [ingress]}
 ---
 apiVersion: metallb.io/v1beta1
 kind: IPAddressPool
@@ -775,10 +775,10 @@ spec:
   serviceAllocation:
     priority: 100
     namespaces:
-      - istio-system
+      - istio-ingress
     serviceSelectors:
       - matchExpressions:
-          - {key: app, operator: In, values: [passthrough-ingressgateway]}
+          - {key: app, operator: In, values: [passthrough]}
 ---
 apiVersion: metallb.io/v1beta1
 kind: L2Advertisement
