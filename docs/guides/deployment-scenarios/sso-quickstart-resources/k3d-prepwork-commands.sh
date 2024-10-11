54daef972aa4cae07c215ee318ddce59bbb85a5d
Wyatt Fry <wfry@bridgephase.com>
2024-10-11 11:57:20 -0400

    wip

--- docs/guides/deployment-scenarios/sso-quickstart-resources/k3d-prepwork-commands.sh
+++ docs/guides/deployment-scenarios/sso-quickstart-resources/k3d-prepwork-commands.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -exuo pipefail
 
-cluster_name="$1"
-
 # Remove the following lines if they already exist
 sudo sed -i "/.*BIG_BANG_VERSION.*/d"      ~/.bashrc
 sudo sed -i "/.*REGISTRY1_USERNAME.*/d"    ~/.bashrc
@@ -10,9 +8,9 @@ sudo sed -i "/.*REGISTRY1_PASSWORD.*/d"    ~/.bashrc
 
 lines_in_file=(
     "export PS1=\"\[\033[01;32m\]\u@${cluster_name}\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "
-    "export CLUSTER_NAME=\"$cluster_name\""
+    "export CLUSTER_NAME=\"$CLUSTER_NAME\""
     "export BIG_BANG_VERSION=\"$BIG_BANG_VERSION\""
-    "export K3D_IP=\"$KEYCLOAK_IP\""
+    "export K3D_IP=\"$K3D_IP\""
     "export REGISTRY1_USERNAME=\"$REGISTRY1_USERNAME\""
     "export REGISTRY1_PASSWORD=\"$REGISTRY1_PASSWORD\""
 )
