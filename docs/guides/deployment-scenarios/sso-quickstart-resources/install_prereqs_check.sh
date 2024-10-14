7303a85cb2e2051de20628b7d4aa4f82f4c910fe
Wyatt Fry <wfry@bridgephase.com>
2024-10-14 11:18:33 -0400

    wip

--- docs/guides/deployment-scenarios/sso-quickstart-resources/install_prereqs_check.sh
+++ docs/guides/deployment-scenarios/sso-quickstart-resources/install_prereqs_check.sh
@@ -4,13 +4,13 @@ set -euo pipefail
 function check() {
   ${*} &> /dev/null \
     && echo "SUCCESS: '${*}' returned non-failure exit code" \
-    || echo -e "\033[31mERROR:   '${*}' returned failure exit code. Verify installation or attempt re-install.\033[0m"
+    || echo -e "\033[31mERROR:   '${*}' returned failure exit code '$?'. Verify installation or attempt re-install.\033[0m"
 }
 
 check_inputs=(
   'docker ps'
   'k3d version'
-  'kubectl version'
+  'which kubectl'
   'kustomize version'
   'helm version'
 )
