0d0ecf8ca35a63a151128483fdce81116b92210c
Wyatt Fry <wfry@bridgephase.com>
2024-10-14 11:25:46 -0400

    wip

--- docs/guides/deployment-scenarios/sso-quickstart-resources/create_k3d_cluster.sh
+++ docs/guides/deployment-scenarios/sso-quickstart-resources/create_k3d_cluster.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 IMAGE_CACHE=${HOME}/.k3d-container-image-cache
 mkdir -p "${IMAGE_CACHE}"
-k3d cluster create "$HOST" \
+k3d cluster create "$(hostname)" \
   --k3s-arg "--tls-san=$K3D_IP@server:0" \
   --volume /etc/machine-id:/etc/machine-id \
   --volume "${IMAGE_CACHE}":/var/lib/rancher/k3s/agent/containerd/io.containerd.content.v1.content \
@@ -13,4 +13,4 @@ k3d cluster create "$HOST" \
   --api-port 6443
 
 # Copy the config replacing 0.0.0.0 with the VM's external IP address so you can access the cluster from your machine
-sed "s/0.0.0.0/$K3D_IP/" ~/.kube/config > "~/.kube/${HOST}-config"
+sed "s/0.0.0.0/$K3D_IP/" ~/.kube/config > "~/.kube/$(hostname)-config"
