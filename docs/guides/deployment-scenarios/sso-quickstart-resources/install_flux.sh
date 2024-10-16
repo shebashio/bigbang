ffad0a536efc98fd7959ad005a2809b699abc646
Wyatt Fry <wfry@bridgephase.com>
2024-10-16 17:31:15 -0400

    wip

--- docs/guides/deployment-scenarios/sso-quickstart-resources/install_flux.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-git clone https://repo1.dso.mil/big-bang/bigbang.git || echo 'directory "bigbang" already exists'
-cd ~/bigbang
-git checkout tags/$BIG_BANG_VERSION
-$HOME/bigbang/scripts/install_flux.sh -u $REGISTRY1_USERNAME -p $REGISTRY1_PASSWORD
