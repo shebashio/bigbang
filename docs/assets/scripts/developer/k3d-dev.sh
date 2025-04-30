c81a249837b9e35b16950657e8fe400f672c9def
Greg <miernicki_gregory@bah.com>
2025-04-30 11:38:56 -0500

    ⛵ revert k3d

--- docs/assets/scripts/developer/k3d-dev.sh
+++ docs/assets/scripts/developer/k3d-dev.sh
@@ -707,7 +707,7 @@ spec:
       - istio-gateway
     serviceSelectors:
       - matchExpressions:
-          - {key: app, operator: In, values: [public-ingressgateway]}
+          - {key: app, operator: In, values: [custom-ingressgateway]}
 ---
 apiVersion: metallb.io/v1beta1
 kind: IPAddressPool
@@ -727,7 +727,7 @@ spec:
       - istio-gateway
     serviceSelectors:
       - matchExpressions:
-          - {key: app, operator: In, values: [custom-ingressgateway]}
+          - {key: app, operator: In, values: [passthrough-ingressgateway]}
 ---
 apiVersion: metallb.io/v1beta1
 kind: L2Advertisement
