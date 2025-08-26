36d583129cf69ddf35f07a5ec53c4bf11c7680a1
dantoomey-omnifederal <dan.toomey@omnifederal.com>
2025-08-26 13:50:26 -0400

    flush out function steps for build, deploy, teardown; test for lingering prior run artifacts; test for arm64

--- docs/assets/configs/zarf/airgap-zarf.sh
+++ docs/assets/configs/zarf/airgap-zarf.sh
@@ -1,10 +1,93 @@
 #!/bin/bash
 
-# this script
-# - parses the results from zarf tools get-creds and substitutes values in the envsubst file.
-# - then creates the zst file
-# - then inspects the file to make sure it worked correctly
+# this script creates a zarf export of bigbang by:
+# - parsing the results from zarf tools get-creds and substitutes values in the envsubst file,
+# - creating the zst file,
+# - and inspecting the file to make sure it worked correctly
 
+ZARF_CREDS_TEMP_FILE="temp file used to store zarf credentials"
+ZARF_PULL="zarf_pull password from zarf credentials"
+ZARF_GIT_USER="zarf_git_user password from zarf credentials"
+
+function make_sure_can_write_local_file() {
+  touch .test_writable_file 2>/dev/null
+  if [ $? -eq 0 ]; then
+    # File can be created in the current directory
+    rm .test_writable_file # Clean up the temporary file
+  else
+    echo "File cannot be created in the current directory (permission denied or other issue)."
+    exit 1
+  fi
+}
+
+# do we need to install docker?
+function start_docker() {
+  if command -v docker &> /dev/null; then
+      echo "Docker is installed."
+  else
+      #echo "Docker is NOT installed."
+      exit 1
+  fi
+
+  if [[ "$(uname -s)" == "Darwin" ]]; then
+    if docker info &> /dev/null; then
+      # "Docker daemon is running."
+      return
+    else
+      # "Docker daemon is not running."
+      open -a Docker
+      until docker version > /dev/null 2>&1; do
+        # "Waiting for Docker daemon to start..."
+        sleep 5
+      done
+      sleep 10
+      return
+    fi
+  else
+    if sudo systemctl is-active --quiet docker; then
+      # "Docker daemon is running."
+      return
+    fi
+
+    # "Docker daemon is not running. Attempting to start Docker..."
+    sudo systemctl start docker
+
+    # Optional: Verify if Docker started successfully
+    if sudo systemctl is-active --quiet docker; then
+      # "Docker daemon started successfully."
+      return
+    fi
+  fi
+
+  echo "Failed to start Docker daemon. Please check logs for errors."
+  exit 1
+}
+
+function create_cluster() {
+  if k3d cluster list | grep -q "mycluster"; then
+     # "k3d cluster '${CLUSTER_NAME}' exists."
+     k3d cluster delete mycluster
+     while k3d cluster list | grep -q "mycluster"; do
+       sleep 5
+     done
+  fi
+
+  k3d cluster create mycluster
+  if [ $? -ne 0 ]; then
+      echo "k3d cluster create failed."
+      exit 1
+  fi
+}
+
+function zarf_init() {
+  zarf init --components=git-server --confirm
+  if [ $? -ne 0 ]; then
+      echo "zarf init failed."
+      exit 1
+  fi
+}
+
+function get_zarf_credentials() {
 #% zarf tools get-creds
 #
 #2025-08-25 14:17:26 INF waiting for cluster connection
@@ -16,34 +99,110 @@
 #     Git (read-only)      | zarf-git-read-user | xPwtiKAw9hreRTdQTgHNGqSd                 | zarf connect git      | git-readonly
 #     Artifact Token       | zarf-git-user      | 97b5254e39c1eec884944b18940929ac22494cee | zarf connect git      | artifact
 
-zarf tools get-creds | sed 's/\x1b\[[0-9;]*[a-zA-Z]//g' > zarf-tools-get-creds.txt
-if [ $? -ne 0 ]; then
-    echo "zarf tools get-creds failed."
-    return $?
-fi
+# remove escape characters in the output for font coloring
+  ZARF_CREDS_TEMP_FILE=$(mktemp)
+  zarf tools get-creds | sed 's/\x1b\[[0-9;]*[a-zA-Z]//g' > "$ZARF_CREDS_TEMP_FILE"
+  if [ $? -ne 0 ]; then
+      echo "zarf tools get-creds failed."
+      exit 1
+  fi
+}
 
-# using pipe delimiter, pull the 3rd column from the row with the username we are looking for
-zarf_pull_plus=$(grep "zarf-pull" zarf-tools-get-creds.txt | cut -d'|' -f3)
-# trim spaces before and after
-ZARF_PULL=$(echo "$zarf_pull_plus" | xargs )
+function extract_passwords() {
+  # using pipe delimiter, pull the 3rd column(Password) from the row with the
+  # Username we are looking for
+  local temp_file="$ZARF_CREDS_TEMP_FILE"
+  if [ -n "$1" ]; then
+    temp_file=$1
+  fi
+  local zarf_pull_plus=$(grep "zarf-pull" "$temp_file" | cut -d'|' -f3)
+  # trim spaces before and after
+  ZARF_PULL=$(echo "$zarf_pull_plus" | xargs )
 
-zarf_git_user_plus=$(grep -m 1 "zarf-git-user" zarf-tools-get-creds.txt | cut -d'|' -f3)
-ZARF_GIT_USER=$(echo "$zarf_git_user_plus" | xargs )
+  local zarf_git_user_plus=$(grep -m 1 "zarf-git-user" "$temp_file" | cut -d'|' -f3)
+  ZARF_GIT_USER=$(echo "$zarf_git_user_plus" | xargs )
+}
 
-export ZARF_PULL
-export ZARF_GIT_USER
-envsubst < bb-zarf-credentials.template.yaml > bb-zarf-credentials.yaml
+function build_zarf_credentials() {
+  # substitute the credentials through the template
+  export ZARF_PULL
+  export ZARF_GIT_USER
+  envsubst < bb-zarf-credentials.template.yaml > bb-zarf-credentials.yaml
+}
 
-zarf package create . --confirm
-if [ $? -ne 0 ]; then
-    echo "zarf package create failed."
-    return $?
-fi
+function create_zarf_package() {
+  # create the zst file, zarf-package-bigbang-amd64.tar.zst
+  # the filename is built from aspects of the credentials file
+  zarf package create . --confirm
+  if [ $? -ne 0 ]; then
+      echo "zarf package create failed."
+      exit 1
+  fi
+}
 
-zarf package inspect definition zarf-package-bigbang-amd64.tar.zst
-if [ $? -ne 0 ]; then
-    echo "zarf package inspection failed."
-    return $?
-fi
+function inspect_zarf_package() {
+  # make sure it worked - inspect the definition portion of the file
+  zarf package inspect definition zarf-package-bigbang-amd64.tar.zst
+  if [ $? -ne 0 ]; then
+      echo "zarf package inspection failed."
+      exit 1
+  fi
+}
 
-return 0
+function deploy_zarf_package() {
+  if [[ "$(uname -m)" == "arm64" ]]; then
+    echo "amd64 package cannot be deployed on arm64"
+    exit 1
+  fi
+  zarf package deploy zarf-package-bigbang-amd64.tar.zst --confirm
+  if [ $? -ne 0 ]; then
+      echo "zarf package deploy failed."
+      exit 1
+  fi
+}
+
+function close_down() {
+  # temp file created on each zarf init
+  local temp_file="$ZARF_CREDS_TEMP_FILE"
+  if [ -n "$1" ]; then
+    temp_file=$1
+  fi
+  rm -f $temp_file
+
+  # credentials file we create to invoke zarf
+  rm -f bb-zarf-credentials.yaml
+
+  # the zarf package we create with bigbang
+  rm -f zarf-package-bigbang-amd64.tar.zst
+
+  # if docker is running
+  if command -v docker &> /dev/null; then
+    # if we created a cluster
+    if k3d cluster list | grep -q "mycluster"; then
+      k3d cluster delete mycluster
+    fi
+  fi
+
+  zarf destroy --confirm
+}
+
+function main() {
+  if [ "$#" -ne 0 ]; then
+    "$1" $2
+  else
+    make_sure_can_write_local_file
+    start_docker
+    create_cluster
+    zarf_init
+    get_zarf_credentials
+    extract_passwords
+    build_zarf_credentials
+    create_zarf_package
+    inspect_zarf_package
+    deploy_zarf_package
+    #close_down
+    echo "AirGap zarf deployment of BigBang complete"
+  fi
+}
+
+main "$@"
